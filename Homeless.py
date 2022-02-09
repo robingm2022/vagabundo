aa0b7473569de018c8bdacc9187f8b655599de6b
Robinson Gutierrez <rgutierrez05@misena.edu.co>
2022-02-09 16:53:33 -0500

    solucion

--- Homeless.py
+++ Homeless.py
@@ -1,6 +1,7 @@
 import random
 
 class Homeless:
+
     def __init__(self, name, x=0, y=0):
         self.name = name
         self.x = x
@@ -13,8 +14,23 @@ class Homeless:
         return (self.x**2 + self.y**2)**0.5
 
 class StandardHomeless(Homeless):
+
     def __init__(self, name):
         super().__init__(name)
 
     def walk(self):
-        return random.choice([(1, 0), (-1, 0), (0, 1), (0, -1)])
+        dx, dy =  random.choice([(0, 2), (0, -2), (2, 0), (-2, 0)])
+        self.x += dx
+        self.y += dy
+        return [self.x, self.y]
+
+class ModerateHomeless(Homeless):
+
+    def __init__(self, name):
+        super().__init__(name)
+
+    def walk(self):
+        dx, dy = random.choice([(0,1), (0,-1), (1,0), (-5,0)])
+        self.x += dx
+        self.y += dy
+        return [self.x, self.y]
